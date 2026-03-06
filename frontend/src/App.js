f16222618408ff55de20c10ef426ca7a178853e7
Yuniscoder <yunishero@gmail.com>
2026-03-06 15:44:34 +0530

    Create main React component for packing optimization system

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Packing Optimization System</h1>
+      {/* Add your components and features here */}
+    </div>
+  );
+};
+
+export default App;
